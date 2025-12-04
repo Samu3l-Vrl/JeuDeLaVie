c4124c5934104a2d0463da7d9736f27c213d6311
Samuel <samuel@Samuel-PC.localdomain>
2025-12-04 19:54:27 +0100

    Ajout de la class aliveCell

--- src/alivecell.cpp
+++ src/alivecell.cpp
@@ -1,1 +1,39 @@
 #include "../headers/alivecell.hpp"
+
+aliveCell::aliveCell() {
+    this->x = x;
+    this->y = y;
+    this->state = true;
+}
+
+aliveCell::aliveCell(int x, int y) {
+    this->x = x;
+    this->y = y;
+    this->state = true;
+}
+
+aliveCell::~aliveCell() {}
+
+int aliveCell::getX() {
+    return this->x;
+}
+
+int aliveCell::getY() {
+    return this->y;
+}
+
+bool aliveCell::getState() {
+    return this->state;
+}
+
+void aliveCell::setX(int x) {
+    this->x = x;
+}
+
+void aliveCell::setY(int y) {
+    this->y = y;
+}
+
+void aliveCell::setState(bool state) {
+    this->state = state;
+}
