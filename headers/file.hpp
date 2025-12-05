d161cc8fad493e0451a59cd94476c7e3d13404f8
Samuel <samuel@Samuel-PC.localdomain>
2025-12-05 15:28:36 +0100

    Modification file

--- headers/file.hpp
+++ headers/file.hpp
@@ -6,6 +6,11 @@ class file{
     public:
         file();
         ~file();
+<<<<<<< HEAD
         void readFile(const std::string& filename, std::vector<std::vector<bool>>& grid);
 
+=======
+        void readFile(const std::string& filename, std::vector<std::vector<int>>& grid);
+        
+>>>>>>> 823ef25 (Modification file)
 };
